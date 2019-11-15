1f13db0b3f5b796c3fc454b5d0d997ae056f3ff2
hgupta10 <himanshu.h.gupta@capgemini.com>
2019-11-15 16:39:15 +0530

    Updates

---
+++ precache-manifest.a4cf2ce6addcc84d0b21ea47c2e800f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fb47a1ad09a8f0317ea56af6539967",
+    "url": "/react_concepts_practise/index.html"
+  },
+  {
+    "revision": "bbe763ecfd24571a034c",
+    "url": "/react_concepts_practise/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "aea0aaa9b547dbe57b45",
+    "url": "/react_concepts_practise/static/css/main.fe4c6da7.chunk.css"
+  },
+  {
+    "revision": "bbe763ecfd24571a034c",
+    "url": "/react_concepts_practise/static/js/2.e4c1029a.chunk.js"
+  },
+  {
+    "revision": "aea0aaa9b547dbe57b45",
+    "url": "/react_concepts_practise/static/js/main.e62c40f6.chunk.js"
+  },
+  {
+    "revision": "956ce377148546b275cf",
+    "url": "/react_concepts_practise/static/js/runtime~main.bb818c7b.js"
+  }
+]);
